ea6620a318d35b191b6928f476b1715ef66588b5
Bugay Nikolay <nikolay.bugay@nixsolutions.com>
2019-10-21 23:16:57 +0300

    Update markup and HTML's title

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,10 @@
 <style>
   table {
     text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
   td {
 		border: 1px solid gray;
@@ -14,25 +18,59 @@
   th {
     background-color: antiquewhite;
   }
+  .top-left-block {
+    width: 50px !important;
+    height: 50px !important;
+    border-color: transparent;
+  }
+  .rotate {
+    transform: rotate(-90deg)
+  }
+  .matrix-body:hover {
+    /* TODO: WIP */
+    transform: scale(2);
+    background: rgb(152, 205, 96);
+    z-index: 334232;
+  }
 </style>
 
 <div>
   <main>
     <table>
       <tr>
-        <td style="border-color: transparent;"></td>
-        <th scope="col">URGENT</th>
-        <th scope="col">NOT URGENT</th>
+        <td class="top-left-block"></td>
+        <th scope="col">
+          <div>Urgent</div>
+        </th>
+        <th scope="col">
+          <div>Less urgent</div>
+        </th>
       </tr>
       <tr>
-        <th scope="row">IMPORTANT</th>
-        <td>John Lennon</td>
-        <td>Rhythm Guitar</td>
+        <th scope="row">
+          <div class="rotate">Important</div>
+        </th>
+        <td class="matrix-body">
+          <div>(Do first)</div>
+          John Lennon
+        </td>
+        <td>
+          <div>(Schedule)</div>
+          Rhythm Guitar
+        </td>
       </tr>
       <tr>
-        <th scope="row">NOT IMPORTANT</th>
-        <td>Paul McCartney</td>
-        <td>Bass</td>
+        <th scope="row">
+          <div class="rotate">Less important</div>
+        </th>
+        <td>
+          <div>(Delegate)</div>
+          Paul McCartney
+        </td>
+        <td>
+          <div>(Don't do)</div>
+          Bass
+        </td>
       </tr>
     </table>
   </main>
